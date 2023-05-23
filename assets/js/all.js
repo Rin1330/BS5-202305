0115858a0937d996321f189b9a5dfa520855fb3f
sakimotorin <sakimotorin@gmail.com>
2023-05-23 17:30:20 +0800

    add footer, finish the dropdown, updated the navbar style

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,3 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+
+  if(document.getElementById('filter')) {
+    const btn = document.querySelector('#filter .dropdown-toggle');
+    const menu = document.querySelector('#filter .dropdown-menu');
+
+    btn.addEventListener('click', () => {
+      if(menu.classList.contains('show')){
+        const item = document.querySelectorAll('#filter .dropdown-item');
+        item.forEach( i => {
+          i.addEventListener('click', (e) => {
+            item.forEach(_item => {
+              _item.classList.remove('active')
+            })
+            e.target.classList.add('active')
+            btn.classList.add('noIcon')
+            btn.textContent = e.target.textContent;
+          })
+        })
+      }
+    })
+  }
+
+  if(document.getElementById('sort-latest')){
+    const btn = document.querySelector('#sort-latest .dropdown-toggle');
+    const menu = document.querySelector('#sort-latest .dropdown-menu');
+    
+    btn.addEventListener('click', () => {
+      if(menu.classList.contains('show')){
+        console.log('menu opened');
+        const item = document.querySelectorAll('#sort-latest .dropdown-item');
+        item.forEach( i => {
+          i.addEventListener('click', e => {
+            btn.textContent = e.target.textContent
+          })
+        })
+      }
+    })
+
+  }
+
 });
